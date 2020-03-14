739fc0c085062f45cbadc7b3abf8393908b7b806
Aleksej Goravskij <32121103+AGoravskiy@users.noreply.github.com>
2020-03-14 13:32:35 +0300

    Initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='context'>
+      <div>
+        <img src='https://www.liberaldictionary.com/wp-content/uploads/2019/01/background-9120.jpg' />
+      </div>
+      <div>
+        ava + discription
+      </div>
+      <div>
+        My product
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
